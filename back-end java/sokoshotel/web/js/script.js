ac32a5b79bdebc7bafd476ece6328361bb7a22af
Kasperi Tuovinen <kasperi.tuovinen@metropolia.fi>
2018-02-26 13:35:43 +0200

    fixed login and username tracking

--- back-end java/sokoshotel/web/js/script.js
+++ back-end java/sokoshotel/web/js/script.js
@@ -1,6 +1,23 @@
 window.onload = function() {
 //Loading JS before HTML
-          
+    
+var username = document.getElementById('username');
+
+	function getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, "\\$&");
+	    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+	// Give the parameter a variable name
+	var dynamicContent = getParameterByName('username');
+ 
+	 $(document).ready(function() {
+             username.innerHTML = dynamicContent;
+	});    
 
 // Get the modal
 var modal = document.getElementById('myModal');
